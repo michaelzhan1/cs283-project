a4197d05ac389bf602b842ad1d959c0f6977bed9
Michael Zhan <michaelzhan2001@gmail.com>
2024-12-13 00:24:31 -0500

    Run real experiment

--- run_april_calibration.py
+++ run_april_calibration.py
@@ -1,12 +1,16 @@
+# NOTE: This script, for some reason, will occasionally fail to run with error
+# "malloc(): mismatching next->prev_size (unsorted)". I believe this is due to
+# something in the AprilTag library.
+# Rerunning the script will eventually work.
+
 from utils.april import check_images, calibrate
 import glob
 
 APRILBOARD_PATH = 'data/AprilBoards.pkl'
 
 def main():
-    images = glob.glob('./data/real/aprilboards/old/img*.jpg')
+    images = glob.glob('./data/real/aprilboards/img*.jpg')
     valid_files = check_images(images, 'coarse', APRILBOARD_PATH)
-    print(valid_files)
     matrix = calibrate(valid_files, 'coarse', APRILBOARD_PATH)
     print(matrix)
 
--- run_real.py
+++ run_real.py
@@ -1,12 +1,31 @@
 import glob
+import numpy as np
 from utils.calibration import calibrate_from_real_images
+from utils.results import get_relative_error, write_results, write_errors
 
 def main():
-    # images = glob.glob('./data/real/scene/scene*.jpg')
-    images = [f'./data/real/scene/img{i}.jpg' for i in range(14)]
-    images += [f'./data/real/scene/img_extra{i}.jpg' for i in range(4)]
-    K = calibrate_from_real_images(images)
-    print(K)
+    # ground truth camera matrix from april calibration
+    K = np.array([
+        [2997, 0, 1553],
+        [0, 2977, 1882],
+        [0, 0, 1]
+    ])
+
+    results = []
+    errors = []
+
+    for i in range(7, 18):
+        print(f"Running on {i} images")
+        max_base_image = min(i, 14)
+        images = [f'./data/real/scene/img{i}.jpg' for i in range(max_base_image)]
+        images += [f'./data/real/scene/img_extra{i}.jpg' for i in range(max(i - 13, 0))]
+        recovered_k = calibrate_from_real_images(images)
+        error = get_relative_error(K, recovered_k)
+        results.append(recovered_k)
+        errors.append(error)
+    
+    write_results(results, 'results/real/results.csv')
+    write_errors(errors, 'results/real/errors.csv')
 
 
 if __name__ == "__main__":
