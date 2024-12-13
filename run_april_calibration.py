a14d9c295e8dbb748b181bb6ef19a2745d48501e
Michael Zhan <michaelzhan2001@gmail.com>
2024-12-13 10:02:28 -0500

    Add warning to apriltag calib

--- run_april_calibration.py
+++ run_april_calibration.py
@@ -9,6 +9,7 @@ import glob
 APRILBOARD_PATH = 'data/AprilBoards.pkl'
 
 def main():
+    print("[WARNING] This script will occasionally fail with error \"malloc(): mismatching next->prev_size (unsorted)\", usually the first time it is run. I believe this is due to something internal within the AprilTag library. Rerunning the script will eventually work.")
     images = glob.glob('./data/real/aprilboards/img*.jpg')
     valid_files = check_images(images, 'coarse', APRILBOARD_PATH)
     matrix = calibrate(valid_files, 'coarse', APRILBOARD_PATH)
